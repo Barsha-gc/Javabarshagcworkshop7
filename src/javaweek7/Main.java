700d03c141779906dae9847df0865c820347d18d
divyanikhatri2007-code <your-email-used-for-this-github>
2026-01-04 20:00:11 +0545

    Added Student, Main, and GrdaeCalculator classes

--- src/javaweek7/Main.java
+++ src/javaweek7/Main.java
@@ -7,7 +7,6 @@ public class Main {
         System.out.println("Welcome to Student Management System!");
 
         Scanner sc = new Scanner(System.in);
-
         System.out.print("Enter student name: ");
         String name = sc.nextLine();
 
@@ -16,5 +15,18 @@ public class Main {
 
         Student student = new Student(name, age);
         System.out.println("Student Details: " + student.getName() + ", Age: " + student.getAge());
+
+        System.out.print("Enter number of grades: ");
+        int n = sc.nextInt();
+        int[] grades = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter grade " + (i + 1) + ": ");
+            grades[i] = sc.nextInt();
+        }
+
+        GradeCalculator gc = new GradeCalculator();
+        double avg = gc.calculateAverage(grades);
+        System.out.println("Average Grade: " + avg);
     }
 }
--- src/javaweek7/Student.java
+++ src/javaweek7/Student.java
@@ -4,13 +4,10 @@ public class Student {
     private String name;
     private int age;
 
-    // Constructor
     public Student(String name, int age) {
         this.name = name;
         this.age = age;
     }
-
-    // Getter methods
     public String getName() {
         return name;
     }
